6231762b9facc8667f9bcc0a22ed2a8eabc92531
Steve Doggett <artigat1@gmail.com>
2025-11-23 12:33:51 +0000

    Add remote pattern for cdn-imgix.headout.com to next image config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'www.gstatic.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn-imgix.headout.com',
+      },
     ],
   },
 };
